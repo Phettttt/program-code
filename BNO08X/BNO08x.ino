906d12f770a16f0198698704a1edf998c36598a6
Thanakorn Sirimaneekul <99063268+Redcomet8300@users.noreply.github.com>
2023-08-28 16:02:09 +0700

    Add files via upload

---
+++ BNO08X/BNO08x.ino
@@ -0,0 +1,32 @@
+void setup() {
+  Serial.begin(9600); //เปลี่ยนได้
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1000); //เขียนต่อ
+}
+n("Could not find a valid BNO08x sensor, check wiring!");
+    while (1);
+  }
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  imu.update();
+
+  if (imu.accelAvailable()) {
+    float accelX = imu.getAccelX();
+    float accelY = imu.getAccelY();
+    float accelZ = imu.getAccelZ();
+
+    Serial.print("Accelerometer: ");
+    Serial.print("X: "); Serial.print(accelX); Serial.print("\t");
+    Serial.print("Y: "); Serial.print(accelY); Serial.print("\t");
+    Serial.print("Z: "); Serial.println(accelZ);
+  }
+
+  delay(100);  // Delay between readings
+}
